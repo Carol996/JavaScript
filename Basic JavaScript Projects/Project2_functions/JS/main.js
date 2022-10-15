3c8fc719a768acd036da06e99416003fb12c8f6b
Carol996 <112996622+Carol996@users.noreply.github.com>
2022-10-15 14:53:38 -0700

    Added new files
    New JS file concatenating two variables using a function.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+var A = "hola"
+var B = "adios"
+
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById('Concatenate').innerHTML = sentence;
+}
+
+function otherFunction() {
+    var sentence = "A second";
+    sentence += " attempt to concatenate variables";
+    document.getElementById('secondTry').innerHTML = sentence;
+}
