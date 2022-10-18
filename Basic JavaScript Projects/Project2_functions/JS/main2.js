31992c82a477c02a2aaf1a1d66fdf382c11b0659
Carol996 <112996622+Carol996@users.noreply.github.com>
2022-10-17 19:28:05 -0700

    Replaced File
    Original files in Project2 were corrupted. I created a new HTML and a new JS file under the names of index2.html and main2.js. I added the number 2 to differentiate between all the other index and js files within my JavaScript folders --avoids confusion.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main2.js
@@ -0,0 +1,7 @@
+//always make sure to put 'onclick' before the function to properly execute it//
+onclick = function func ()    {
+    var sent = "this is an example";
+    sent += " of concatenation with plus-equal formula.";
+document.getElementById("plusEq").innerHTML = sent;//no space before .innerHTML//
+}
+
