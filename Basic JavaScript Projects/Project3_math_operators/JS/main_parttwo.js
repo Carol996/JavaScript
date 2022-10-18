3a2c5334741a78d3881c7889bc91d19a403748df
Carol996 <112996622+Carol996@users.noreply.github.com>
2022-10-18 01:16:49 -0700

    Created Project 3

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main_parttwo.js
@@ -0,0 +1,14 @@
+var J = 5;
+J++;
+document.write(J) ;
+
+//shows as 64.25 on browser because there's no spacing between these functions//
+var K = 5.25;
+K--;
+document.write(K);
+
+window.alert(Math.random()); //returns a random number between 0 and 1//
+
+window.alert(Math.random() * 100); //random number between __ and i.e. 100//
+
+window.alert((Math.min (2, 3, 1) ));
