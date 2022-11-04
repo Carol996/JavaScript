d91a19447413e285a0456229288e726e04f8c458
Carol996 <112996622+Carol996@users.noreply.github.com>
2022-11-04 16:35:00 -0700

    addtl Challenges

---
+++ Basic JavaScript Projects/Final_Challenges/PopUp_Form/js/popup.js
@@ -0,0 +1,7 @@
+function openForm() { //this function tells the element ided as myForm to display
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() { // this function tells the element ided as myForm to 'not display' --in this case, hide
+    document.getElementById("myForm").style.display = "none";
+  }
